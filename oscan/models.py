6c44d44b4c6478c6fd1a1d39c6b64f08b7e37058
hduisa <hduisa@hdu.edu.cn>
2013-07-13 11:25:09 +0800

    Database

--- oscan/models.py
+++ oscan/models.py
@@ -5,19 +5,18 @@ from django.utils import timezone
 
 # Create your models here.
 class Bugs(models.Model):
-    btype = models.CharField(max_length=200)
+    type = models.CharField(max_length=200,primary_key=True)
     name = models.CharField(max_length=200)
+    description=models.CharField(max_length=200)
     level = models.IntegerField()
     suggestion = models.CharField(max_length=200)
 
     def __unicode__(self):
         return str(self.btype)
 
-    
-
 class Tasks(models.Model):
-    userid = models.IntegerField()
-    taskname = models.CharField(max_length=200)
+    taskid=models.IntegerField(primary_key=True)
+    userid= models.IntegerField()
     starttime = models.DateTimeField()
     finishtime = models.DateTimeField()
     status = models.CharField(max_length=200)
@@ -25,16 +24,15 @@ class Tasks(models.Model):
 
     def __unicode__(self):
         return str(self.taskname)
-    
 
 class Results(models.Model):
-    task = models.ForeignKey(Tasks)
-    numberintask = models.IntegerField()
-    bugtype = models.CharField(max_length=200)
+    taskid = models.ForeignKey(Tasks)
+    numberintask = models.IntegerField(primary_key=True)
+    bugtype = models.ForeignKey(Bugs)
     detail = models.CharField(max_length=200)
-    bugid = models.IntegerField()
     
     def __unicode__(self):
         return str(self.task) + str(self.bugid)
 
 
+    
